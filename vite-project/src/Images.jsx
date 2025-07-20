01fce3be42b259536505b4c27ebdb7fee84972c8
TamilNangaiK <k.tamilpirai@gmail.com>
2025-07-20 10:24:38 +0530

    Images file

--- vite-project/src/Images.jsx
+++ vite-project/src/Images.jsx
@@ -1,40 +1,125 @@
 import "./Images.css"
-import one from "./assets/i1.png"
-import two from "./assets/i2.png"
-import three from "./assets/i3.png"
-import four from "./assets/i4.png"
-import five from "./assets/i5.png"
-import six from "./assets/i6.png"
-import seven from "./assets/i7.png"
-import eight from "./assets/i8.png"
-import nine from "./assets/i9.png"
-import ten from "./assets/i10.png"
-import eleven from "./assets/i11.png"
-import twelve from "./assets/i12.png"
-import thirteen from "./assets/i13.png"
-import fourteen from "./assets/i14.png"
-import fifteen from "./assets/i15.png"
+import Done from "./assets/imageDv/i1.png"
+import Dtwo from "./assets/imageDv/i2.png"
+import Dthree from "./assets/imageDv/i3.png"
+import Dfour from "./assets/imageDv/i4.png"
+import Dfive from "./assets/imageDv/i5.png"
+import Dsix from "./assets/imageDv/i6.png"
+import Dseven from "./assets/imageDv/i7.png"
+import Deight from "./assets/imageDv/i8.png"
+import Dnine from "./assets/imageDv/i9.png"
+import Dten from "./assets/imageDv/i10.png"
+import Deleven from "./assets/imageDv/i11.png"
+import Dtwelve from "./assets/imageDv/i12.png"
+import Dthirteen from "./assets/imageDv/i13.png"
+import Dfourteen from "./assets/imageDv/i14.png"
+import Dfifteen from "./assets/imageDv/i15.png"
+
+
+
+import Tone from "./assets/imageTv/i1.png"
+import Ttwo from "./assets/imageTv/i2.png"
+import Tthree from "./assets/imageTv/i3.png"
+import Tfour from "./assets/imageTv/i4.png"
+import Tfive from "./assets/imageTv/i5.png"
+import Tsix from "./assets/imageTv/i6.png"
+import Tseven from "./assets/imageTv/i7.png"
+import Teight from "./assets/imageTv/i8.png"
+import Tnine from "./assets/imageTv/i9.png"
+import Tten from "./assets/imageTv/i10.png"
+import Televen from "./assets/imageTv/i11.png"
+import Ttwelve from "./assets/imageTv/i12.png"
+import Tthirteen from "./assets/imageTv/i13.png"
+import Tfourteen from "./assets/imageTv/i14.png"
+import Tfifteen from "./assets/imageTv/i15.png"
+
+
+
+import Mone from "./assets/imageMv/i1.png"
+import Mtwo from "./assets/imageMv/i2.png"
+import Mthree from "./assets/imageMv/i3.png"
+import Mfour from "./assets/imageMv/i4.png"
+import Mfive from "./assets/imageMv/i5.png"
+import Msix from "./assets/imageMv/i6.png"
+import Mseven from "./assets/imageMv/i7.png"
+import Meight from "./assets/imageMv/i8.png"
+import Mnine from "./assets/imageMv/i9.png"
+import Mten from "./assets/imageMv/i10.png"
+import Meleven from "./assets/imageMv/i11.png"
+import Mtwelve from "./assets/imageMv/i12.png"
+import Mthirteen from "./assets/imageMv/i13.png"
+import Mfourteen from "./assets/imageMv/i14.png"
+import Mfifteen from "./assets/imageMv/i15.png"
+import Msixteen from "./assets/imageMv/i16.png"
+
+
 function Images() {
   return (
-    <div id="flex-container">
-        <div class="flex-item"><img src={one} alt="i1" /></div>
-        <div class="flex-item"><img src={two} alt="i2" /></div>
-        <div class="flex-item"><img src={three} alt="i3" /></div>
-        <div class="flex-item"><img src={four} alt="i4" /></div>
-        <div class="flex-item"><img src={five} alt="i6" /></div>
-        <div class="flex-item"><img src={six} alt="i7" /></div>
-        <div class="flex-item"><img src={seven} alt="i8" /></div>
-        <div class="flex-item"><img src={eight} alt="i9" /></div>
-        <div class="flex-item"><img src={nine} alt="i10" /></div>
-        <div class="flex-item"><img src={ten} alt="i11" /></div>
-        <div class="flex-item"><img src={eleven} alt="i5" /></div>
-        <div class="flex-item"><img src={twelve} alt="i12" /></div>
-        <div class="flex-item"><img src={thirteen} alt="i13" /></div>
-        <div class="flex-item"><img src={fourteen} alt="i14" /></div>
-        <div class="flex-item"><img src={fifteen} alt="i15" /></div>
-        
 
-    </div>
+    <>
+      <div id="flex-containerD">
+        <div class="flex-item"><img src={Done} alt="i1" /></div>
+        <div class="flex-item"><img src={Dtwo} alt="i2" /></div>
+        <div class="flex-item"><img src={Dthree} alt="i3" /></div>
+        <div class="flex-item"><img src={Dfour} alt="i4" /></div>
+        <div class="flex-item"><img src={Dfive} alt="i6" /></div>
+        <div class="flex-item"><img src={Dsix} alt="i7" /></div>
+        <div class="flex-item"><img src={Dseven} alt="i8" /></div>
+        <div class="flex-item"><img src={Deight} alt="i9" /></div>
+        <div class="flex-item"><img src={Dnine} alt="i10" /></div>
+        <div class="flex-item"><img src={Dten} alt="i11" /></div>
+        <div class="flex-item"><img src={Deleven} alt="i5" /></div>
+        <div class="flex-item"><img src={Dtwelve} alt="i12" /></div>
+        <div class="flex-item"><img src={Dthirteen} alt="i13" /></div>
+        <div class="flex-item"><img src={Dfourteen} alt="i14" /></div>
+        <div class="flex-item"><img src={Dfifteen} alt="i15" /></div>
+
+
+      </div>
+
+
+      <div id="flex-containerT">
+        <div class="flex-item"><img src={Tone} alt="i1" /></div>
+        <div class="flex-item"><img src={Ttwo} alt="i2" /></div>
+        <div class="flex-item"><img src={Tthree} alt="i3" /></div>
+        <div class="flex-item"><img src={Tfour} alt="i4" /></div>
+        <div class="flex-item"><img src={Tfive} alt="i6" /></div>
+        <div class="flex-item"><img src={Tsix} alt="i7" /></div>
+        <div class="flex-item"><img src={Tseven} alt="i8" /></div>
+        <div class="flex-item"><img src={Teight} alt="i9" /></div>
+        <div class="flex-item"><img src={Tnine} alt="i10" /></div>
+        <div class="flex-item"><img src={Tten} alt="i11" /></div>
+        <div class="flex-item"><img src={Televen} alt="i5" /></div>
+        <div class="flex-item"><img src={Ttwelve} alt="i12" /></div>
+        <div class="flex-item"><img src={Tthirteen} alt="i13" /></div>
+        <div class="flex-item"><img src={Tfourteen} alt="i14" /></div>
+        <div class="flex-item"><img src={Tfifteen} alt="i15" /></div>
+
+
+      </div>
+
+
+      <div id="flex-containerM">
+        <div class="flex-item"><img src={Mone} alt="i1" /></div>
+        <div class="flex-item"><img src={Mtwo} alt="i2" /></div>
+        <div class="flex-item"><img src={Mthree} alt="i3" /></div>
+        <div class="flex-item"><img src={Mfour} alt="i4" /></div>
+        <div class="flex-item"><img src={Mfive} alt="i6" /></div>
+        <div class="flex-item"><img src={Msix} alt="i7" /></div>
+        <div class="flex-item"><img src={Mseven} alt="i8" /></div>
+        <div class="flex-item"><img src={Meight} alt="i9" /></div>
+        <div class="flex-item"><img src={Mnine} alt="i10" /></div>
+        <div class="flex-item"><img src={Mten} alt="i11" /></div>
+        <div class="flex-item"><img src={Meleven} alt="i5" /></div>
+        <div class="flex-item"><img src={Mtwelve} alt="i12" /></div>
+        <div class="flex-item"><img src={Mthirteen} alt="i13" /></div>
+        <div class="flex-item"><img src={Mfourteen} alt="i14" /></div>
+        <div class="flex-item"><img src={Mfifteen} alt="i15" /></div>
+        <div class="flex-item"><img src={Msixteen} alt="i16" /></div>
+
+
+      </div>
+    </>
   )
 }
 
