3baf91f2a189ff673796ef09ad51c03a14fb32df
TamilNangaiK <k.tamilpirai@gmail.com>
2025-07-20 15:44:51 +0530

    modified files

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,15 +1,15 @@
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import WhyChooseUs from './WhyChooseUs.jsx'
-
+import Images from './images.jsx'
+import Contactus from './Contactus.jsx'
+import Hero from './Hero.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     
-    <WhyChooseUs />
-
-    
+  <Hero/>
+   
   </StrictMode>
 )
