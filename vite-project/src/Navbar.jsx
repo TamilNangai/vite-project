e29181c4547fc3a06518b6d3ac60669c50a8e78e
TamilNangaiK <k.tamilpirai@gmail.com>
2025-07-21 19:25:58 +0530

    navbar

---
+++ vite-project/src/Navbar.jsx
@@ -0,0 +1,20 @@
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        
+    <header>
+        <nav>           
+            <span id='head'>SAFE GUARD TARPS</span>         
+            <ul id="nav-items">                
+                <li id='home'> <a href="#Home"> Home </a> </li>
+                <li id='about'> <a href="#About"> About </a> </li>
+                <li id='product'> <a href="#Product"> Product</a> </li>
+                <li id='contact'> <a href="#Conduct"> Contact us</a> </li>               
+            </ul>
+        </nav>
+    </header>
+    )
+}
+
+export default Navbar
